d1693566302bea0e5a3d29c6101ef9a1bf9eb877
swdt1 <swdt1@cms-r6-62.cms.waikato.ac.nz>
2016-09-10 14:24:14 +1200

    Add print statement to message recieving, find_channel() function

--- src/servers/fs/subserv.c
+++ src/servers/fs/subserv.c
@@ -10,8 +10,25 @@
 #include <stddef.h>
 #include <string.h>
 
+#include <stdio.h>
+
 struct channel *channels = NULL;
 
 PUBLIC int do_subserv() {
   /* TODO: check message status code, act accordingly */
+  printf("Message reached server\n");
+}
+
+struct channel* find_channel(char *cname) {
+  struct channel *curr = channels;
+  
+  while (curr != NULL) {
+    if (strncmp(cname, &curr->cname, 14) == 0) {
+      break;
+    }
+    
+    curr = curr->next;
+  }
+  
+  return curr;
 }
